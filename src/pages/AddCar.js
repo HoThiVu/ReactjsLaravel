b25b916a28c05f9893ad7e6c8452a89bd4107ca4
HoThiVu <89339469+HoThiVu@users.noreply.github.com>
2022-07-09 11:56:25 +0700

    day bai create

--- src/pages/AddCar.js
+++ src/pages/AddCar.js
@@ -1,127 +1,167 @@
 import { tab } from '@testing-library/user-event/dist/tab';
 import { createRef, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Table } from 'reactstrap';
-import React, { Component } from 'react';
-import axios from axios;
-import { Modal, ModalBody, ModalHeader, Button, FormGroup, Input, Label, ModalFooter } from 'reactstrap';
-const AddCar = (props) => {
+
+import axios from 'axios';
+import { Modal, ModalBody, ModalHeader, Button, FormGroup, Input, Label, ModalFooter, Form } from 'reactstrap';
+
+const AddCar = ({onAdded}) => {
     const [modal, setModal] = useState(false);
     const [car, setCar] = useState({});
-    const toogle = () => {
-        setModal(!modal);
+
+    // const handleShow = () => {
+    //     setModal(!modal)
+    // }
+    const handleCloseShow = () => {
+        setModal(!modal)
     }
-
-
 
     const handlerInput = (e) => {
         const { name, value } = e.target;
         console.log(car);
         setCar({
             ...car,
+            [name]: value,
+        })
+    }
+    const handleImageFile = (e) => {
+        setCar({
+            ...car,
             file: e.target.files && e.target.files.length ? URL.createObjectURL(e.target.files[0]) : car.file,
             image: e.target.files && e.target.files.length ? e.target.files[0].name : car.image,
-            [name]: value,
         })
     }
 
     const handleSubmitForm = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:8000/api/cars', {
-            firstName: 'Fred',
-            lastName: 'Flintstone'
-        })
-            .then(function (response) {
-                console.log(response);
+        // e.preventDefault();
+        // const fileInput = document.querySelector('#fileupload');
+        // const formData = new FormData();
+        // formData.append('image', fileInput.files[0]);
+        // formData.append('màu', car.màu);
+        // formData.append('hãng', car.hãng);
+        // formData.append('produced_on', car.produced_on);
+        // fetch('http://localhost:8000/api/cars', { method: 'post', body: formData })
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         toogle();
+        //         onRedirect();
+        //     })
+
+        e.preventDefault();
+        const fileInput = document.querySelector('#fileUpload');
+        const formData = new FormData();
+        formData.append('hãng', car.hãng);
+        formData.append('màu', car.màu);
+        formData.append('produced_on', car.produced_on);
+        formData.append('image', fileInput.files[0]);
+    
+        axios.post('http://localhost:8000/api/cars', formData)
+            .then((response) => {
+                console.log(response.data);
+                onRedirect();
+
             })
             .catch(function (error) {
                 console.log(error);
-            });
-
+            })
+         
+          
+    };
+    const onRedirect = () => {
+        setCar({});
+        handleCloseShow();
+        onAdded(false)
     }
     return (
         <div>
             <Button
                 color="danger"
-                onClick={toogle}
+                onClick={handleCloseShow}
             >
                 ADD A NEW CAR
             </Button>
-            <Modal isOpen={modal} toggle={toogle}
-
+            <Modal isOpen={modal} toggle={handleCloseShow}
             >
-                <ModalHeader toggle={toogle}>
+
+                <ModalHeader toggle={handleCloseShow}>
                     Modal title
                 </ModalHeader>
+
                 <ModalBody>
-                    {/* <Form onSubmit={onSubmit}> */}
-                    <FormGroup>
-                        <Label for="exampleEmail">
-                            màu
-                        </Label>
-                        <Input
-                            id="exampleEmail"
-                            name="màu"
-                            value={car.màu}
-                            onChange={handlerInput}
-                            placeholder="with a placeholder"
-                            type="text"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleEmail">
-                            hãng
-                        </Label>
-                        <Input
-                            id="exampleEmail"
-                            name="hãng"
-                            value={car.hãng}
-                            onChange={handlerInput}
-                            placeholder="with a placeholder"
-                            type="text"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleEmail">
-                            image
-                        </Label>
-                        <Input
-                            id="exampleEmail"
-                            name="image"
-                            value={car.image}
-                            onChange={handlerInput}
-                            placeholder="with a placeholder"
-                            type="file"
-                        />
-                        <img alt='ts' src={car.file}></img>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleEmail">
-                            produced_on
-                        </Label>
-                        <Input
-                            id="exampleEmail"
-                            name="produced_on"
-                            value={car.produced_on}
-                            onChange={handlerInput}
-                            placeholder="with a placeholder"
-                            type="date"
-                        />
-                    </FormGroup>
-                    {/* </Form> */}
+                    <Form onSubmit={handleSubmitForm}>
+                        <FormGroup>
+                            <Label for="exampleEmail">
+                                hang
+                            </Label>
+                            <Input
+                                id="exampleEmail"
+                                name="hãng"
+                                // value={car.màu}
+                                onChange={handlerInput}
+                                placeholder="with a placeholder"
+                                type="text"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="exampleEmail">
+                                màu
+                            </Label>
+                            <Input
+                                id="exampleEmail"
+                                name="màu"
+                                // value={car.hãng}
+                                onChange={handlerInput}
+                                placeholder="with a placeholder"
+                                type="text"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="exampleEmail">
+                                image
+                            </Label>
+                            <Input
+                                id="fileUpload"
+                                name="fileUpload"
+                                // value={car.image}
+                                onChange={handleImageFile}
+                                placeholder="with a placeholder"
+                                type="file"
+                            />
+                            <img alt='ts' src={car.file} style={{ with: '10em', height: '100px' }}></img>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="exampleEmail">
+                                produced_on
+                            </Label>
+                            <Input
+                                id="exampleEmail"
+                                name="produced_on"
+
+                                onChange={handlerInput}
+                                placeholder="with a placeholder"
+                                type="date"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Button
+                                color="primary"
+                                type='submit'
+                            >
+                                Save
+                            </Button>
+                            <Button 
+                            onClick={handleCloseShow} >
+                                Cancel
+                            </Button>
+                        </FormGroup>
+                    </Form>
+
                 </ModalBody>
                 <ModalFooter>
-                    <Button
-                        color="primary"
-                        onClick={handleSubmitForm}
-                    >
-                        Save
-                    </Button>
 
-                    <Button onClick={toogle} >
-                        Cancel
-                    </Button>
                 </ModalFooter>
+
             </Modal>
         </div>
     );
--- src/pages/CarList.js
+++ src/pages/CarList.js
@@ -4,19 +4,19 @@ import React, { useEffect, useState } from "react";
 import { Table } from 'reactstrap';
 import { StyleListCar } from './../css/StyleListCar.css';
 import AddCar from './AddCar';
-
-
-
-
+import EditCar from './EditCar.js';
 const CarList = (props) => {
     const [cars, setCars] = useState([]);
+    const [isLoaded,setIsLoaded]= useState(false)
     const getCars = () => {
+
         // Make a request for a user with a given ID
         axios.get('http://localhost:8000/api/cars')
             .then(function (response) {                
                 // handle success
                 console.log(response);
                 setCars(response.data.data)
+                setIsLoaded(true)
             })
             .catch(function (error) {
                 // handle error
@@ -28,66 +28,154 @@ const CarList = (props) => {
     }
 
 useEffect(() =>{
-    getCars()
-},[])
+    if (!isLoaded) getCars();
+},[isLoaded])
+
+
+// import axios from "axios";
+// import AddCar from './AddCar';
+// import EditCar from './EditCar';
+
+
+// const CarList = () => {
+//     // const [isLoaded,setIsLoaded]= useState(false)
+//     const [cars, setCars] = useState([{
+//         id: "",
+//         hãng: "",
+//         màu: "",
+//         produced_on: "",
+//         image: ""
+//     }]);  //cars là 1 mảng các đối tượng
+//     const [noDataFound, setNoDataFound] = useState("");
+
+//     //gọi hàm này khi CarList được render lần đầu (chú ý tham số thứ 2 là mảng rỗng). Hàm này như hàm lifecycle componentDidMount()
+//     useEffect(() => {
+//         getCars();
+//         //             if (!isLoaded) getCars();
+//         // },[isLoaded]
+//         //console.log("hello carlist");
+//     }, []);
+
+//     //cập nhật lại state cars
+//     const getCars = () => {
+//         axios.get('http://localhost:8000/api/cars').then((res) => {
+//             console.log(res.data);
+//             if (res.status === 200) {
+//                 setCars(res.data.data ? res.data.data : []); //c là biến tự đặt tên gì cũng đc, ko có nó thì in ra cars=[]
+//                 console.log(res.data.data ? res.data.data : []);
+//                 console.log(cars);//vì cars lúc này chỉ render 
+//                 // setIsLoaded(true)
+//             };
+//             if (res.data.status === "failed" && res.data.success === false) {
+//                 setNoDataFound(res.data.message);
+//                 console.log(noDataFound);
+//             };
+//         }).catch((error) => {
+//             console.log(error);
+//         });
+//     };
+
+//     //bắt đầu làm cho EditCar.js
+//     const [editmodal, setEditModal] = useState(false);
+
+//     const [editCarData, setEditCarData] = useState({
+//         id: "",
+//         hãng: "",
+//         màu: "",
+//         produced_on: "",
+//         // manufacturer:"",
+//         image: ""
+//     });
+
+//     //editCarData là 1 mảng các đối tượng lưu dữ liệu đối tượng Car được edit
+//     //hàm này nhận biến car ở sự kiện onClick() của nút Sửa
+//     const toggleEditModal = (car) => {
+//         setEditModal(!editmodal);
+//         if (editmodal === false) setEditCarData(car);  //phải có kiểm tra điều kiện form edit được mở thì mới cập nhật không thì sẽ lỗi không nhận ra editCarData.manufacturer.id khi đóng form
+//         console.log(editCarData);
+//     }
+//     //tham số car này là tham số đầu vào
+//     const deleteCar = (id) => {
+//         axios.delete('http://localhost:8000/api/cars' + id)  //tham số truyền vào là id
+//             .then((res) => {
+//                 console.log("Car removed deleted");
+//                 getCars();
+//             }).catch((error) => {
+//                 console.log(error)
+//             })
+//     }
+
+    //đóng delete
+
     return (
-        <div class="table-responsive">
-            <div class="table-wrapper">
-                <div class="table-title">
-                    <div class="row">
-                        <div class="col-xs-5">
+        <div className="table-responsive">
+            <div className="table-wrapper">
+                <div className="table-title">
+                    <div className="row">
+                        <div className="col-xs-5">
                             <h2>Admin <b>ListCar</b></h2>
-                            <AddCar></AddCar>
+                            {/* <AddCar onAdded={setIsLoaded}/> */}
+                            <AddCar  onAdded={setIsLoaded}></AddCar>
+                            {/* <AddCar getCars={getCars} /> */}
+                            {/* <Link to="/cars/add"><Button color="primary">Add car page</Button></Link> */}
+
+                            {/* <EditCar cars={cars} setCars={setCars} getCars={getCars} toggleEditModal={toggleEditModal} editmodal={editmodal} editCarData={editCarData} setEditCarData={setEditCarData} /> */}
                         </div>
-                       
+
                     </div>
                 </div>
-                <table class="table table-striped table-hover">
+                <table className="table table-striped table-hover">
                     <thead>
                         <tr>
                             <th>ID</th>
                             <th>Ảnh</th>
 
-                            <th>Hãng xe</th>
-                            <th>Màu xe</th>
-                            {/* <th>Tên nsx</th> */}
+                            <th>hang xe</th>
+                            <th>mau xe</th>
+
                             <th>NSX</th>
                             <th>Công cụ</th>
                         </tr>
                     </thead>
-                   
+
                     <tbody>
                         {cars.length !== 0
-                        ?
-                     cars.map((car,index)=>
-                   
-                        <tr key={index}>
-                            <td>{car.id}</td>
-                            <td><a href="#">
-                                <img  width="100px"
-                                height="100px" class="avatar" src={`http://localhost:8000/img/${car.image}`} alt="Avatar" /></a></td>
-                            <td>{car.hãng}</td>
-                            <td>{car.màu}</td>
-                            <td>{car.produced_on}</td>
-                            {/* <td>{car.id}</td> */}
-                            <td>
-                                <a href="" class="settings" title="EDIT"
-                                    data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
-                                <button type="submit" onclick="return confirm('chắc chắn bạn muons xóa không')"
-                                    class="settings" title="delete" data-toggle="tooltip"><i
-                                        class="material-icons">&#xE5C9;</i>
-                                </button>
-                            </td>
-                        </tr>
-                )
-                :
-                <tr>
-                    <td>{'No data in database'}</td>
-                    
-                </tr> }
+                            ?
+                            cars.map((car, index) =>
+
+                                <tr key={index}>
+                                    <td>{car.id}</td>
+                                    <td><a href="#">
+                                        <img width="100px"
+                                            height="100px" className="avatar" src={`http://localhost:8000/img/${car.image}`} alt="Avatar" /></a></td>
+                                    <td>{car.hãng}</td>
+                                    <td>{car.màu}</td>
+                                    <td>{car.produced_on}</td>
+
+                                    <td>
+                                        <a href="" className="settings" title="EDIT"
+                                            data-toggle="tooltip">
+                                            <i className="material-icons">&#xE8B8;</i>
+
+                                        </a>
+                                        <button type="submit"
+                                                className="settings" 
+                                                title="delete" 
+                                                data-toggle="tooltip" 
+                                         > <i
+                                                className="material-icons">&#xE5C9;</i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                            :
+                            <tr>
+                                <td>{'No data in database'}</td>
+
+                            </tr>}
 
                     </tbody>
-               
+
                 </table>
 
             </div>
